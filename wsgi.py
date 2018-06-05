7c0a6bf0938ae56595f7014ce8a4e668630b1748
rafaelln <rafael_limanascimento@yahoo.com.br>
2018-06-05 13:16:50 -0300

    r test

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     ip = os.environ.get('OPENSHIFT_PYTHON_IP', '0.0.0.0')
     port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8051))
 
-    # Para detalhae melhor os Logs no OpenShift
+    # Para detalhae melhor os Logs no OpenShift r
     application.config['PROPAGATE_EXCEPTIONS'] = True
 
     run(ip, port)
